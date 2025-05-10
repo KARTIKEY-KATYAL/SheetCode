bd5cddf74d78fac0a1a3fe8e9aa573e105f82a07
Kartikey Katyal <kartikeykatyal2003@gmail.com>
2025-05-10 21:30:14 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,33 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.tsx'
-import Home from './Home.tsx'
-import { ThemeProvider } from './components/ThemeProvider.tsx'
-import SignUp from './components/SignUp.tsx'
-import LogIn from './components/LogIn.tsx'
-import Problems from './components/Problems.tsx'
-import Profile from './components/Profile.tsx'
-import Sheets from './components/Sheets.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,Routes } from 'react-router-dom';
+import './index.css';
+
+import Layout from './Layout.tsx';
+import Home from './Home.tsx';
+import { ThemeProvider } from './components/ThemeProvider.tsx';
+import SignUp from './components/SignUp.tsx';
+import LogIn from './components/LogIn.tsx';
+import Problems from './components/Problems.tsx';
+import Profile from './components/Profile.tsx';
+import Sheets from './components/Sheets.tsx';
+
+const authuser = null
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='signup' element ={<SignUp/>}/>
-      <Route path='login' element = {<LogIn/>}/>
-      <Route path='problems' element = {<Problems/>}/>
-      <Route path='sheets' element = {<Sheets/>}/>
-      <Route path='profile' element = {<Profile/>}/>
-      
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<LogIn />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="problems" element={<Problems />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="sheets" element={<Sheets />} />
     </Route>
   )
-)
+);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="sheetcode-theme">
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
